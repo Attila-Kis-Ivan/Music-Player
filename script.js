a8a58629ed21e68bff74f67e1757ce9308a0efa5
TDB <attila.kisivan@gmail.com>
2022-12-24 10:07:57 +0000

    added progress bar and control buttons functionality

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ const nextBtn = document.getElementById('next');
 // Music
 const songs = [
     {
-      name: 'tdb-1',
+      name: 'TDB-1',
       displayName: 'Electric Chill Machine',
       artist: 'TDB-Design',
     },
     {
-      name: 'tdb-2',
+      name: 'TDB-2',
       displayName: 'Seven Nation Army (Remix)',
       artist: 'TDB-Design',
     },
     {
-      name: 'tdb-3',
+      name: 'TDB-3',
       displayName: 'Goodnight, Disco Queen',
       artist: 'TDB-Design',
     },
@@ -61,11 +61,79 @@ const songs = [
 
 //   Update Dome
 function loadSong(song) {
-    title.textContent = song.displayName;
-    artist.textContent = song.artist;
-    music.src = `music/${song.name}.mp3`;
-    image.src = `img/${song.name}.jpg`;
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
 }
 
-// On load
-loadSong(songs[1]);
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Next Song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Update progressbar
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const {duration, currentTime} = e.srcElement;
+    // Update progress bar
+    const progressPrecent = (currentTime / duration) *100;
+    progress.style.width = `${progressPrecent}%`;
+    //alculate duration time
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if(durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+    //Deley switching duration elemet to avoid nan
+    if(durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+    //alculate current time
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if(currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  }
+}
+
+//
+function setProgressBar(e){
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const {duration} = music;
+  console.log(clickX / width);
+  console.log((clickX / width)* duration);
+  music.currentTime = (clickX / width)* duration;
+}
+
+
+// Event listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong)
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
